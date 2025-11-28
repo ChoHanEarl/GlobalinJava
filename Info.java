eb122e13340f6dccdec94e0014e7d68c47410662
ChoHanEarl <159342404+ChoHanEarl@users.noreply.github.com>
2025-11-28 14:07:21 +0900

    Info, HelloWorld
    터미널을 통한 자바의 파일 생성 및 실행

---
+++ Info.java
@@ -0,0 +1,21 @@
+public class Info {
+    public static void main(String[] args){
+        System.out.println("프로그래밍 언어 종류");
+        System.out.println("1. 컴파일");
+        System.out.println("2. 인터프리터");
+
+        System.out.println("디렉토리 이동");
+        System.out.println("cd <경로>");
+
+        System.out.println("디렉토리 생성");
+        System.out.println("mkdir");
+
+        System.out.println("자바 버전 확인");
+        System.out.println("cd <java파일 경로/bin>");
+        System.out.println("java --version");
+
+        System.out.println("컴파일");
+        System.out.println("javac HelloWorld.java"); // .class파일이 생성됨
+        System.out.println("java HelloWorld"); // 파일 실행
+    }
+}
