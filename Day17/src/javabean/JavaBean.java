2679d087b66f14c502cdddddfcab93c96ff1ddba
ChoHanEarl <159342404+ChoHanEarl@users.noreply.github.com>
2025-12-23 13:05:17 +0900

    JavaBean 復習

---
+++ Day17/src/javabean/JavaBean.java
@@ -0,0 +1,21 @@
+package javabean;
+
+public class JavaBean {
+	
+	// 정보
+	private int bean;
+	
+	// 생성자
+	public JavaBean() {}
+	
+	// 처리
+	public int getBean() {
+		return this.bean;
+	}
+	
+	public void setBean(int bean) {
+		this.bean = bean;
+	}
+	
+	
+}
